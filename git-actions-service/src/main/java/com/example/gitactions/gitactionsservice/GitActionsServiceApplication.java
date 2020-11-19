9d38d71eaa0df2508688a92ef0a4e61b82af5b3c
Joel Schrock <Joel.Schrock@kohls.com>
2020-11-19 14:11:46 -0600

    initial commit

---
+++ git-actions-service/src/main/java/com/example/gitactions/gitactionsservice/GitActionsServiceApplication.java
@@ -0,0 +1,20 @@
+package com.example.gitactions.gitactionsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GitActionsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitActionsServiceApplication.class, args);
+	}
+
+	@GetMapping("/action")
+	public String getActions(){
+		return "Go Actions Go";
+	}
+}
---
+++ git-actions-service/src/test/java/com/example/gitactions/gitactionsservice/GitActionsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitactions.gitactionsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActionsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
